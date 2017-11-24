0286959bf6177081f5e274dae9cf934816733744
arifromadhan19 <arifromadhan19@gmail.com>
2017-11-24 18:11:31 +0700

    add linked list

--- simple_linked_list.py
+++ simple_linked_list.py
@@ -1,5 +1,5 @@
 class Node:
-    def __init__(self, data):
+    def __init__(self, data=None, next=None):
         self.data = data
         self.next = None
 
@@ -14,7 +14,7 @@ class LinkedList:
             print (temp.data),
             temp = temp.next
 
-    def Insert(self, data):
+    def InsertTailNode(self, data):
 
         if (self.head == None):
             head = Node(data)
@@ -25,11 +25,34 @@ class LinkedList:
             current.next = Node(data)
         return self.head
 
+    def InsertHeadNode(self,data):
+        current = Node(data)
+        current.next = self.head
+        self.head = current
+
+    def InsertSpecificPosition(self,data,position):
+        if not position == 0:
+            current = self.head
+            current_position = 1
+            while (position - current_position > 0):
+                current = current.next
+                current_position += 1
+            if current.next is None:
+                current.next = Node(data, None)
+                return self.head
+            else:
+                prev = current.next
+                current.next = Node(data, prev)
+                return self.head
+        else:
+            return Node(data, self.head)
+
 
 if __name__ == '__main__':
     llist = LinkedList()
 
-    llist.head = Node(1)
+    # llist.head = Node(1)
+    #Insert Manual
     # second = Node(2)
     # third = Node(3)
     #
@@ -37,7 +60,10 @@ if __name__ == '__main__':
     # second.next = third
 
     #Insert by function
-    llist.Insert(2)
-    llist.Insert(3)
+    llist.InsertHeadNode(1)
+    llist.InsertSpecificPosition(2,1)
+    llist.InsertSpecificPosition(3, 2)
+    # llist.InsertTailNode(2)
+    # llist.InsertTailNode(3)
 
     llist.printList()
