2cf3c24791c017a9385d09f871f89c5a707bc008
arifromadhan19 <arifromadhan19@gmail.com>
2017-11-24 18:23:36 +0700

    add linked list

--- simple_linked_list.py
+++ simple_linked_list.py
@@ -47,6 +47,17 @@ class LinkedList:
         else:
             return Node(data, self.head)
 
+    def Delete(self, position):
+        temp = self.head
+        if position == 0:
+            return temp.next
+
+        while position - 1 > 0:
+            head = self.head.next
+            position -= 1
+        self.head.next = self.head.next.next
+        return temp
+
 
 if __name__ == '__main__':
     llist = LinkedList()
@@ -55,7 +66,6 @@ if __name__ == '__main__':
     #Insert Manual
     # second = Node(2)
     # third = Node(3)
-    #
     # llist.head.next = second
     # second.next = third
 
@@ -63,7 +73,8 @@ if __name__ == '__main__':
     llist.InsertHeadNode(1)
     llist.InsertSpecificPosition(2,1)
     llist.InsertSpecificPosition(3, 2)
-    # llist.InsertTailNode(2)
-    # llist.InsertTailNode(3)
+    llist.InsertTailNode(4)
+
+    # llist.Delete(2)
 
     llist.printList()
